2a1c0d6e2e00086f0d2d3fc52f58048fa75669cc
cora <945793560@qq.com>
2025-10-24 12:39:40 +0800

    resume

--- launch_warp_and_sdxl_training.sh
+++ launch_warp_and_sdxl_training.sh
@@ -14,8 +14,9 @@ accelerate launch --num_processes 2 train_controlnet_flux_warp.py\
  --gradient_accumulation_steps=32 \
  --gradient_checkpointing \
  --dataloader_num_workers 8 \
- --mixed_precision="bf16"
-#  --resume_from_checkpoint='/data/output_sdxl_large_dataset_warp/checkpoint-32500'
+ --mixed_precision="bf16" \
+ --resume_from_checkpoint='../data/output_flux_warp_large_dataset/checkpoint-3000' \
+ --checkpointing_steps=3000
 #  --validation_image "/data/result_sphere/stitch/2025-03-07_11-20-44/render_h_transformed_hsi.mat" "/home/viplab/diffusers/examples/controlnet/mytry/dataset/result/2025-02-10_11-01-49"
 #  --validation_prompt "city view"\
 #  --max_train_steps=6000 \
